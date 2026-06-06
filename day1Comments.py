883a5ce50642bf061d8a88642fcc0c742d711eab
andym <amejia9149@bths.edu>
2026-06-05 21:36:33 -0400

    day 1 of python

---
+++ day1Comments.py
@@ -0,0 +1,19 @@
+"""Dictionary:
+i never heard of this but it is like a hashmap, a  key value pair format
+first_name: asabenth
+Tuple: sounds like a throple
+an ordered collection of different data types like list but tuples can't be modified
+immutable such as const
+('Asabeneth', 'Pawel', 'Brook')
+Set:
+A collection of data types similar to list and tupe
+set is not an ordered # of items
+Examples:>>> type(10)
+<class 'int'>
+>>> type(1+3j)
+<class 'complex'>
+>>> type({'name',3})
+<class 'set'>
+>>>
+"""
+
---
+++ helloworld.py
@@ -0,0 +1,4 @@
+print({'andy', 3, 4.5})
+# The euclidean distance between (2,3) and (10,8)
+print(type((10 - 3) ** 2 - (8 - 2) ** 2))
+print(((10 - 3) ** 2 - (8 - 2) ** 2))
